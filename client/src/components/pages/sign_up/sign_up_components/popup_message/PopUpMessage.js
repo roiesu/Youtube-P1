0f3218d27d98f5bb3ef5c318eeae9f6fcadd105a
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-05-29 12:06:31 +0300

    Finised the style of sign up page

--- client/src/components/pages/sign_up/sign_up_components/popup_message/PopUpMessage.js
+++ client/src/components/pages/sign_up/sign_up_components/popup_message/PopUpMessage.js
@@ -3,14 +3,11 @@ import "./PopUpMessage.css";
 function PopUpMessage(props) {
   return (
     <div className="message-anchor">
-      {props.isActive ? <div className="popup-message">{props.message}</div> : ""}
+      <div className={props.isActive ? "popup-message visible" : "popup-message hidden"}>
+        {props.message}
+      </div>
     </div>
   );
-  // return (
-  //   <div className="message-anchor">
-  //     <div className="popup-message">{props.message}</div>
-  //   </div>
-  // );
 }
 
 export default PopUpMessage;
