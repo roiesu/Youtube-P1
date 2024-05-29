6881568f1f860dcd7545fddb1e8c321a8abd13ce
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-05-29 12:06:31 +0300

    Finised the style of sign up page

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from "react";
 import "./App.css";
 import SignUp from "../pages/sign_up/SignUp";
+import SignIn from "../pages/sing_in/SignIn";
 
 function App() {
   const [users, setUsers] = useState([]);
   const [currentUser, setCurrentUser] = useState();
-  console.log("users", users);
-  console.log("current", currentUser);
   return (
     <div className="App">
       <SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />
+      <SignIn />
     </div>
   );
 }
