b248937140e7fb7f7a42e7747dd883a6f44db609
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-05-31 11:40:03 +0300

    added routes

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,17 +1,26 @@
 import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignUp from "../pages/sign_up/SignUp";
 import SignIn from "../pages/sign_in/SignIn";
 import usersList from "../../data/users.json";
+
 function App() {
   const [users, setUsers] = useState(usersList);
   const [currentUser, setCurrentUser] = useState();
   console.log(users);
   return (
-    <div className="App">
-      <SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />
-      {/* <SignIn /> */}
-    </div>
+    <Router basename="/">
+      <div className="App">
+        <Routes>
+          <Route
+            element={<SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />}
+            path="/sign-up"
+          />
+          <Route element={<SignIn />} path="/sign-in" />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
