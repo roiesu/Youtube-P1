061c287ae44e047d3ac90fe1f83aaf7219f14b90
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-02 18:10:30 +0300

    Added moving between pages

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -3,24 +3,43 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import SignUp from "../pages/sign_up/SignUp";
 import SignIn from "../pages/sign_in/SignIn";
+import MainPage from "../pages/main_page/MainPage";
 import usersList from "../../data/users.json";
+import Bar from "../pages/general_components/bar/Bar";
+import WatchVideoPage from "../pages/watch_video/WatchVideoPage";
+import UploadVideoPage from "../pages/upload_video/UploadVideoPage";
 
 function App() {
   const [users, setUsers] = useState(usersList);
   const [currentUser, setCurrentUser] = useState();
-  console.log(users);
   return (
-    <Router basename="/">
-      <div className="App">
+    <div className="App">
+      <Router>
         <Routes>
-          <Route
-            element={<SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />}
-            path="/sign-up"
-          />
-          <Route element={<SignIn />} path="/sign-in" />
+          {/* Pages anyone can see */}
+          <Route exact path="/" element={<MainPage />} />
+          <Route exact path="/watch/:id" element={<WatchVideoPage />} />
+          {currentUser ? (
+            // Pages only users can see
+            <>
+              <Route path="/upload" element={<UploadVideoPage />} />
+            </>
+          ) : (
+            <>
+              {/* Pages only non users can see */}
+              <Route
+                element={
+                  <SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />
+                }
+                exact
+                path="/sign-up"
+              />
+              <Route element={<SignIn />} exact path="/sign-in" />
+            </>
+          )}
         </Routes>
-      </div>
-    </Router>
+      </Router>
+    </div>
   );
 }
 
