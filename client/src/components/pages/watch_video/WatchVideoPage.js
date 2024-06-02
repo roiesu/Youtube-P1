121c5576a590bd39cb27e8624357561b8ac2b70a
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-02 19:12:43 +0300

    A working video watching page

--- client/src/components/pages/watch_video/WatchVideoPage.js
+++ client/src/components/pages/watch_video/WatchVideoPage.js
@@ -1,9 +1,28 @@
-import React from 'react'
-
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { findVideoById } from "../../../utilities";
 function WatchVideoPage() {
+  const [video, setVideo] = useState();
+  const { id } = useParams();
+  useEffect(() => {
+    if (!id) return;
+    const found = findVideoById(id);
+    console.log(found);
+    setVideo(found);
+  }, [id]);
+
   return (
-    <div>WatchVideoPage</div>
-  )
+    <div>
+      <h1>WatchVideoPage</h1>
+      {video ? (
+        <video controls>
+          <source src={video.src} type="video/mp4" />
+        </video>
+      ) : (
+        "Video not found"
+      )}
+    </div>
+  );
 }
 
-export default WatchVideoPage
+export default WatchVideoPage;
