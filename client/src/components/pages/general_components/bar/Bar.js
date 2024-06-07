c2ce5620f0ea53a810e5ff747b96b2e55daa59ae
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-07 15:04:33 +0300

    Added the sidebar

--- client/src/components/pages/general_components/bar/Bar.js
+++ client/src/components/pages/general_components/bar/Bar.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 function Bar() {
-  return (
-    <div>Bar</div>
-  )
+  const location = useLocation();
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    if (location.pathname.match("sign")) {
+      setVisible(false);
+      return;
+    }
+    setVisible(true);
+  }, [location]);
+
+  return visible ? <div>Bar</div> : "";
 }
 
-export default Bar
+export default Bar;
