e72fc4040e90d974eb764e40dfe17169eae5d124
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-07 16:58:53 +0300

    added  a search bar and auto paly

--- client/src/components/pages/watch_video/comments/Comments.js
+++ client/src/components/pages/watch_video/comments/Comments.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { callWithEnter } from "../../../../utilities";
 
 function Comments({ comments, addComment, commentInput }) {
   return (
@@ -6,11 +7,18 @@ function Comments({ comments, addComment, commentInput }) {
       <div className="comments-title">
         {comments.length} {comments.length === 1 ? "Comment" : "Comments"}
       </div>
-      {comments.map((comment) => (
-        <div className="comment">{`<${comment.date_time}> ${comment.user}: ${comment.text}`}</div>
-      ))}
-      <input id="comment-input" name="comment" ref={commentInput} />
+      <input
+        id="comment-input"
+        name="comment"
+        ref={commentInput}
+        onKeyDown={(e) => callWithEnter(e, addComment)}
+      />
       <button onClick={addComment}>Add</button>
+      <div className="comments-list">
+        {comments.map((comment) => (
+          <div className="comment">{`<${comment.date_time}> ${comment.user}: ${comment.text}`}</div>
+        ))}
+      </div>
     </div>
   );
 }
