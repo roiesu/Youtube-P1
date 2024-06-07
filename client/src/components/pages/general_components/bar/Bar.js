efac389288392bbb1ae57701e008c8cbf8fa782b
roiesu <roiesu3@gmail.com>
2024-06-07 18:18:56 +0300

    side bar

--- client/src/components/pages/general_components/bar/Bar.js
+++ client/src/components/pages/general_components/bar/Bar.js
@@ -1,18 +1,37 @@
 import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+import '../bar.css';
 
 function Bar() {
   const location = useLocation();
   const [visible, setVisible] = useState(false);
+
   useEffect(() => {
-    if (location.pathname.match("sign")) {
+    if (!location.pathname.match("sign")) {
+      setVisible(true);
+    } else {
       setVisible(false);
-      return;
     }
-    setVisible(true);
   }, [location]);
 
-  return visible ? <div>Bar</div> : "";
+  const toggleSidebar = () => {
+    setVisible(!visible);
+  };
+
+  return (
+    <div className="sidebar-container">
+      <button className="sidebar-toggle" onClick={toggleSidebar}>
+        {visible ? 'Close' : 'Open'}
+      </button>
+      {visible && (
+        <div className="sidebar">
+          <div href="#" className="sidebar-link">Home</div>
+          <div href="#" className="sidebar-link">MyVideos</div>
+          <div href="#" className="sidebar-link">Subscriptions</div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default Bar;
