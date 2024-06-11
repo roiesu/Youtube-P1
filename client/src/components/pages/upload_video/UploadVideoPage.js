36e8b512fa839747503e0e74ab5b1f5265d127e0
yuvalOz11 <137378916+yuvalOz11@users.noreply.github.com>
2024-06-11 11:57:18 +0300

    my videos page

--- client/src/components/pages/upload_video/UploadVideoPage.js
+++ client/src/components/pages/upload_video/UploadVideoPage.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './UploadVideo.css'
 
 function UploadVideoPage() {
   return (
-    <div>UploadVideoPage</div>
+    <div className='video-details'>
+      <div class="name">
+        <input type="text" class="form-control" placeholder="pr-name" aria-label="video-name" aria-describedby="basic-addon1" />
+      </div>
+
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text">With textarea</span>
+        </div>
+        <textarea class="form-control" aria-label="With textarea"></textarea>
+      </div>
+
+    </div>
   )
 }
 
