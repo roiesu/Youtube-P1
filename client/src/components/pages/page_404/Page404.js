7fac230843721f9eead434b64df17f3580e61ddc
roiesu <roiesu3@gmail.com>
2024-06-11 11:53:09 +0300

    page 404 design

--- client/src/components/pages/page_404/Page404.js
+++ client/src/components/pages/page_404/Page404.js
@@ -1,17 +1,20 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import "./Page404.css";
 function Page404() {
   const navigate = useNavigate();
+
   useEffect(() => {
     setTimeout(() => {
       navigate("/");
     }, 3000);
-  });
+  }, [navigate]);
+
   return (
     <div className="page-404 page">
-      Page not found!
-      <br /> Redirecting to main page.
+      <h1>404</h1>
+      <p>Page not found!</p>
+      <div className="redirect-message">Redirecting to main page...</div>
     </div>
   );
 }
