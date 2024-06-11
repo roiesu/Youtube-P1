a1c888a2731e47516eb273008e7af4aa59aa3c41
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-11 12:27:45 +0300

    added the theme context

--- client/src/components/app/App.js
+++ client/src/components/app/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Link } from "react-router-dom";
 
@@ -12,55 +12,61 @@ import Bar from "../pages/general_components/bar/Bar";
 import WatchVideoPage from "../pages/watch_video/WatchVideoPage";
 import UploadVideoPage from "../pages/upload_video/UploadVideoPage";
 import Page404 from "../pages/page_404/Page404";
+import { ThemeContext } from "../pages/general_components/ThemeContext";
 
 function App() {
   const [users, setUsers] = useState(usersList);
   const [currentUser, setCurrentUser] = useState(usersList[0]);
   const [videos, setVideos] = useState(videoList);
-
   return (
-    <div className="App">
-      <Router>
-        <div className="img">
-          <Link to="/">
-            <img src=" ../../../logo.png" />
-          </Link>
-        </div>
-        <Bar logout={() => setCurrentUser(null)} loggedIn={currentUser != null} />
-        <Routes>
-          {/* Pages anyone can see */}
-          <Route exact path="/" element={<MainPage videos={videos} currentUser={currentUser} />} />
-          <Route
-            exact
-            path="/watch/:v?"
-            element={<WatchVideoPage videos={videos} currentUser={currentUser} />}
-          />
-          {currentUser ? (
-            // Pages only users can see
-            <>
-              <Route path="/upload" element={<UploadVideoPage />} />
-            </>
-          ) : (
-            <>
-              {/* Pages only non users can see */}
-              <Route
-                element={
-                  <SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />
-                }
-                exact
-                path="/sign-up"
-              />
-              <Route
-                element={<SignIn users={users} setCurrentUser={setCurrentUser} />}
-                exact
-                path="/sign-in"
-              />
-            </>
-          )}
-          <Route path="*" element={<Page404 />} />
-        </Routes>
-      </Router>
-    </div>
+    <ThemeContext>
+      <div className="App">
+        <Router>
+          <div className="img">
+            <Link to="/">
+              <img src=" ../../../logo.png" />
+            </Link>
+          </div>
+          <Bar logout={() => setCurrentUser(null)} loggedIn={currentUser != null} />
+          <Routes>
+            {/* Pages anyone can see */}
+            <Route
+              exact
+              path="/"
+              element={<MainPage videos={videos} currentUser={currentUser} />}
+            />
+            <Route
+              exact
+              path="/watch/:v?"
+              element={<WatchVideoPage videos={videos} currentUser={currentUser} />}
+            />
+            {currentUser ? (
+              // Pages only users can see
+              <>
+                <Route path="/upload" element={<UploadVideoPage />} />
+              </>
+            ) : (
+              <>
+                {/* Pages only non users can see */}
+                <Route
+                  element={
+                    <SignUp users={users} setCurrentUser={setCurrentUser} setUsers={setUsers} />
+                  }
+                  exact
+                  path="/sign-up"
+                />
+                <Route
+                  element={<SignIn users={users} setCurrentUser={setCurrentUser} />}
+                  exact
+                  path="/sign-in"
+                />
+              </>
+            )}
+            <Route path="*" element={<Page404 />} />
+          </Routes>
+        </Router>
+      </div>
+    </ThemeContext>
   );
 }
 
