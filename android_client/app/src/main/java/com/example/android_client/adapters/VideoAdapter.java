d435ca431ca20d4bfdcdc029d646eaa3796248b7
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-16 10:54:35 +0300

    initializing

--- android_client/app/src/main/java/com/example/android_client/adapters/VideoAdapter.java
+++ android_client/app/src/main/java/com/example/android_client/adapters/VideoAdapter.java
@@ -1,7 +1,13 @@
 package com.example.android_client.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.example.android_client.R;
 import com.example.android_client.entities.Video;
@@ -9,9 +15,15 @@ import com.example.android_client.entities.Video;
 import java.util.ArrayList;
 
 public class VideoAdapter extends ArrayAdapter<Video> {
+    private LayoutInflater inflater;
 
     public VideoAdapter(Context context, ArrayList<Video> videos) {
         super(context, R.layout.video_link_item,videos);
-
+        this.inflater=LayoutInflater.from(context);
+    }
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
+        Video video = getItem(position);
+        
     }
 }
