703929940b38918b93aed54a716c6650a30eebf2
roiesu <roiesu3@gmail.com>
2024-06-16 15:18:25 +0300

    changing branch
    //

--- android_client/app/src/main/java/com/example/android_client/adapters/VideoAdapter.java
+++ android_client/app/src/main/java/com/example/android_client/adapters/VideoAdapter.java
@@ -4,44 +4,59 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.VideoView;
-
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
+import androidx.recyclerview.widget.RecyclerView;
 import com.example.android_client.R;
 import com.example.android_client.entities.Video;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
 
-public class VideoAdapter extends ArrayAdapter<Video> {
-    private LayoutInflater inflater;
+public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHolder> {
+    private Context context;
+    private ArrayList<Video> videos;
 
     public VideoAdapter(Context context, ArrayList<Video> videos) {
-        super(context, R.layout.video_link_item,videos);
-        this.inflater=LayoutInflater.from(context);
+        this.context = context;
+        this.videos = videos;
     }
-    @Override
-    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent){
-        Video video = getItem(position);
-        if(convertView == null){
-            convertView = inflater.inflate(R.layout.video_link_item,parent,false);
-        }
-        VideoView videoPreview = (VideoView) convertView.findViewById(R.id.videoPreview);
-        TextView videoTitle = (TextView) convertView.findViewById(R.id.videoTitle);
-        TextView videoUploader = (TextView) convertView.findViewById(R.id.videoUploader);
-        TextView videoViews = (TextView) convertView.findViewById(R.id.videoViews);
-        TextView videoDate = (TextView) convertView.findViewById(R.id.videoDate);
 
-        videoPreview.setVideoPath(video.getSrc());
-        videoTitle.setText(video.getName());
-        videoUploader.setText(video.getDisplayUploader());
-        videoDate.setText(video.getDate_time().toString());
-        videoViews.setText(video.getViews());
-        return convertView;
+    @NonNull
+    @Override
+    public VideoViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.video_link_item, parent, false);
+        return new VideoViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull VideoViewHolder holder, int position) {
+        Video video = videos.get(position);
+        holder.videoPreview.setVideoPath(video.getSrc());
+        holder.videoTitle.setText(video.getName());
+        holder.videoUploader.setText(video.getDisplayUploader());
+        holder.videoViews.setText(String.valueOf(video.getViews()));
+        holder.videoDate.setText(video.getDate_time().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return videos.size();
+    }
+
+    public static class VideoViewHolder extends RecyclerView.ViewHolder {
+        VideoView videoPreview;
+        TextView videoTitle;
+        TextView videoUploader;
+        TextView videoViews;
+        TextView videoDate;
+
+        public VideoViewHolder(@NonNull View itemView) {
+            super(itemView);
+            videoPreview = itemView.findViewById(R.id.videoPreview);
+            videoTitle = itemView.findViewById(R.id.videoTitle);
+            videoUploader = itemView.findViewById(R.id.videoUploader);
+            videoViews = itemView.findViewById(R.id.videoViews);
+            videoDate = itemView.findViewById(R.id.videoDate);
+        }
     }
 }
