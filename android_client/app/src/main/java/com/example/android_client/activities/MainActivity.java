a13998a3414a225d1f8a0f39d09bc0b44aa53a0d
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-17 17:58:17 +0300

    more merege fixes
    (committed while working together in a zoom call)

--- android_client/app/src/main/java/com/example/android_client/activities/MainActivity.java
+++ android_client/app/src/main/java/com/example/android_client/activities/MainActivity.java
@@ -39,24 +39,9 @@ public class MainActivity extends AppCompatActivity {
             DataManager.setCurrentUser(DataManager.getUsersList().get(0));
             DataManager.setInitialized(true);
         }
-
-        setContentView(R.layout.activity_main);
-        view = findViewById(R.id.main_page);
-        view.setOnClickListener(view ->{
-            Intent intent = new Intent(this, SignUp.class);
-            startActivity(intent);
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-
-        });
-        findViewById(R.id.mainButton).setOnClickListener(view->{
             Intent intent = new Intent(this, MainPage.class);
             startActivity(intent);
-        });
-        DataManager.printUsers();
+
     }
     private <T> ArrayList<T> loadDataFromJson(String fileName, Type type) {
         ArrayList<T> array = new ArrayList<>();
