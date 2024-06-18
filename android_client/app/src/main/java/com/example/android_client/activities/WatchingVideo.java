1098745b5a91719494c1c724496a5b610781e546
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-18 09:21:45 +0300

    testing share

--- android_client/app/src/main/java/com/example/android_client/activities/WatchingVideo.java
+++ android_client/app/src/main/java/com/example/android_client/activities/WatchingVideo.java
@@ -1,6 +1,9 @@
 package com.example.android_client.activities;
+
 import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.android_client.R;
 
 import android.content.Intent;
@@ -8,6 +11,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -18,8 +22,11 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.android_client.R;
+import com.example.android_client.Utilities;
 import com.example.android_client.adapters.CommentAdapter;
+import com.example.android_client.entities.Comment;
 import com.example.android_client.entities.DataManager;
+import com.example.android_client.entities.User;
 import com.example.android_client.entities.Video;
 
 import java.util.ArrayList;
@@ -31,58 +38,98 @@ public class WatchingVideo extends AppCompatActivity {
     private TextView commentsHeader;
 
     private Button likeButton;
+    private Button commentButton;
+    private EditText commentInput;
+    private User currentUser;
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent = getIntent();
-        int id = intent.getIntExtra("videoId", 0);
+        String appLinkAction = intent.getAction();
+        int id = 0;
+        if (appLinkAction == null) {
+            id = intent.getIntExtra("videoId", 0);
+        } else {
+            Log.w("Bish","HERE");
+            String lastPathSegment = intent.getData().getLastPathSegment();
+            Log.w("Bash",lastPathSegment);
+            if (lastPathSegment.matches("\\d+")) {
+                id = Integer.parseInt(lastPathSegment);
+            }
+        }
         video = DataManager.findVideoById(id, true);
         if (video == null) {
             setContentView(R.layout.video_not_found_layout);
             return;
         }
-        setContentView(R.layout.watching_video);
-        ((TextView) findViewById(R.id.videoTitle)).setText(video.getName());
-        ((TextView) findViewById(R.id.videoViews)).setText("" + video.getViews());
-        ((TextView) findViewById(R.id.videoDate)).setText("" + video.getDate_time());
-        ((TextView) findViewById(R.id.videoDescription)).setText(video.getDescription());
-        ((TextView) findViewById(R.id.videoUploader)).setText(video.getDisplayUploader());
-        likeButton = findViewById(R.id.likeButton);
-        likeButton.setText(video.getLikes().size()+"");
-        likeButton.setOnClickListener(view -> {
-            if(DataManager.getCurrentUser()==null){
+            setContentView(R.layout.watching_video);
+            ((TextView) findViewById(R.id.videoTitle)).setText(video.getName());
+            ((TextView) findViewById(R.id.videoViews)).setText("" + video.getViews());
+            ((TextView) findViewById(R.id.videoDate)).setText(Utilities.formatDate(video.getDate_time()));
+            ((TextView) findViewById(R.id.videoDescription)).setText(video.getDescription());
+            ((TextView) findViewById(R.id.videoUploader)).setText(video.getDisplayUploader());
+            commentInput = findViewById(R.id.commentInput);
+            commentButton = findViewById(R.id.commentButton);
+            likeButton = findViewById(R.id.likeButton);
+            likeButton.setText(video.getLikes().size() + "");
+            likeButton.setOnClickListener(view -> {
+                if (DataManager.getCurrentUser() == null) {
+                    return;
+                }
+                DataManager.likeVideo(video.getId(), DataManager.getCurrentUser().getUsername());
+                ((TextView) view).setText(video.getLikes().size() + "");
+            });
+            videoView = initVideo();
+            videoView.start();
+
+            commentsList = findViewById(R.id.commentsList);
+            commentsList.setLayoutManager(new LinearLayoutManager(this));
+            CommentAdapter adapter = new CommentAdapter(this, video.getComments());
+            commentsList.setAdapter(adapter);
+
+            commentsHeader = findViewById(R.id.commentsTitle);
+            commentsHeader.setText(adapter.getItemCount() + " Comments");
+
+            currentUser = DataManager.getCurrentUser();
+            commentButton.setOnClickListener(l -> {
+                commentVideo(adapter);
+            });
+
+
+            // ATTENTION: This was auto-generated to handle app links.
+//        Intent appLinkIntent = getIntent();
+//        String appLinkAction = appLinkIntent.getAction();
+//        Uri appLinkData = appLinkIntent.getData();
+        }
+
+        private Uri getVideoURI () {
+            int videoResId = getResources().getIdentifier(video.getSrc(), "raw", getPackageName());
+            String uriString = "android.resource://" + getPackageName() + "/" + videoResId;
+            return Uri.parse(uriString);
+        }
+
+        private VideoView initVideo () {
+            VideoView temp = findViewById(R.id.videoView);
+            temp.setVideoURI(getVideoURI());
+            MediaController mediaController = new MediaController(this);
+            mediaController.setAnchorView(temp);
+            temp.setMediaController(mediaController);
+            return temp;
+        }
+
+        private void commentVideo (CommentAdapter adapter){
+            if (currentUser == null) {
+                // Notify user
                 return;
             }
-            DataManager.likeVideo(video.getId(), DataManager.getCurrentUser().getUsername());
-            ((TextView)view).setText(video.getLikes().size()+"");
-        });
-        videoView = initVideo();
-        videoView.start();
-
-        commentsList = findViewById(R.id.commentsList);
-        commentsList.setLayoutManager(new LinearLayoutManager(this));
-        CommentAdapter adapter = new CommentAdapter(this, video.getComments());
-        commentsList.setAdapter(adapter);
-
-        commentsHeader = findViewById(R.id.commentsTitle);
-        commentsHeader.setText(adapter.getItemCount() + " Comments");
-
-
+            String content = commentInput.getText().toString();
+            if (content.matches("^[\\s \\r\\t\\n]*$")) {
+                // Notify user
+                return;
+            }
+            video.addComment(currentUser.getUsername(), currentUser.getName(), content);
+            commentInput.setText("");
+            adapter.notifyDataSetChanged();
+            commentsHeader.setText(adapter.getItemCount() + " Comments");
+        }
     }
-
-    private Uri getVideoURI() {
-        int videoResId = getResources().getIdentifier(video.getSrc(), "raw", getPackageName());
-        String uriString = "android.resource://" + getPackageName() + "/" + videoResId;
-        return Uri.parse(uriString);
-    }
-
-    private VideoView initVideo() {
-        VideoView temp = findViewById(R.id.videoView);
-        temp.setVideoURI(getVideoURI());
-        MediaController mediaController = new MediaController(this);
-        mediaController.setAnchorView(temp);
-        temp.setMediaController(mediaController);
-        return temp;
-
-    }
-}
