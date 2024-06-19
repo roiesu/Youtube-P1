e59fb7cba9a029212a6ef3aa71f1b67edfefa7be
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-19 15:50:58 +0300

    almost done

--- android_client/app/src/main/java/com/example/android_client/activities/SignIn.java
+++ android_client/app/src/main/java/com/example/android_client/activities/SignIn.java
@@ -1,13 +1,55 @@
 package com.example.android_client.activities;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
 import com.example.android_client.R;
+import com.example.android_client.adapters.InputValidationAdapter;
+import com.example.android_client.entities.DataManager;
+import com.example.android_client.entities.InputValidation;
+import com.example.android_client.entities.User;
+
+import java.util.ArrayList;
 
 
 public class SignIn extends AppCompatActivity {
+    private ArrayList<InputValidation> inputs;
+    private RecyclerView inputList;
+    private Button submit;
+    private TextView errorView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sign_in);
+        inputList = findViewById(R.id.recyclerView);
+        inputList.setLayoutManager(new LinearLayoutManager(this));
+        inputs = new ArrayList<>();
+        String [] inputNames =getResources().getStringArray(R.array.inputNames);
+        String [] inputRegex =getResources().getStringArray(R.array.inputRegex);
+        String [] inputReqs =getResources().getStringArray(R.array.inputRequierments);
+        for(int i=0;i<2;i++){
+            inputs.add(new InputValidation(inputNames[i],inputRegex[i],inputReqs[i]));
+        }
+        InputValidationAdapter adapter = new InputValidationAdapter(this,inputs,false);
+        inputList.setAdapter(adapter);
+        submit = findViewById(R.id.submit);
+        submit.setOnClickListener(l->{login();});
+        errorView = findViewById(R.id.validationError);
+    }
+    private void login(){
+        User user= DataManager.findUser(inputs.get(0).getInputText());
+        if(user==null||!user.getPassword().equals(inputs.get(1).getInputText())){
+            errorView.setText("Uset not found");
+            return;
+        }
+        DataManager.setCurrentUser(user);
+        Intent intent = new Intent(this,MainPage.class);
+        startActivity(intent);
     }
 }
---
+++ android_client/app/src/main/java/com/example/android_client/adapters/InputValidationAdapter.java
@@ -0,0 +1,77 @@
+package com.example.android_client.adapters;
+
+import static android.text.InputType.TYPE_CLASS_TEXT;
+import static android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD;
+import static android.text.InputType.TYPE_TEXT_VARIATION_WEB_PASSWORD;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.TooltipCompat;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.android_client.R;
+import com.example.android_client.entities.InputValidation;
+
+import java.util.ArrayList;
+
+public class InputValidationAdapter extends RecyclerView.Adapter<InputValidationAdapter.InputHolder> {
+
+    private Context context;
+    private ArrayList<InputValidation> inputs;
+    private boolean showTooltips;
+
+    public InputValidationAdapter(Context context, ArrayList<InputValidation> inputs, boolean showTooltips) {
+        this.context = context;
+        this.inputs = inputs;
+        this.showTooltips = showTooltips;
+    }
+
+    @NonNull
+    @Override
+    public InputHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.input_validation, parent, false);
+        return new InputHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull InputHolder holder, int position) {
+        InputValidation inputValidation = inputs.get(position);
+        inputValidation.setInput(holder.input);
+        holder.label.setText(inputValidation.getName());
+        holder.input.setHint(inputValidation.getName());
+        if (inputValidation.getName().matches(".*(p|P)assword.*")) {
+            holder.input.setInputType(TYPE_CLASS_TEXT|TYPE_TEXT_VARIATION_PASSWORD
+            );
+        }
+        if (showTooltips) {
+            TooltipCompat.setTooltipText(holder.details, inputValidation.getReqs());
+        } else {
+            holder.details.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return inputs.size();
+    }
+
+    public class InputHolder extends RecyclerView.ViewHolder {
+        private EditText input;
+        private TextView label;
+        private ImageView details;
+
+        public InputHolder(@NonNull View itemView) {
+            super(itemView);
+            this.details = itemView.findViewById(R.id.inputDetails);
+            this.label = itemView.findViewById(R.id.inputLabel);
+            this.input = itemView.findViewById(R.id.editInput);
+        }
+    }
+}
