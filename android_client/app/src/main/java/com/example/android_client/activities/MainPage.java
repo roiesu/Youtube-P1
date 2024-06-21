2c1ccf0659531fd5f63537126108be61d1bee1c4
roiesu <roiesu3@gmail.com>
2024-06-21 15:04:10 +0300

    dark mode unfinished
    //

--- android_client/app/src/main/java/com/example/android_client/activities/MainPage.java
+++ android_client/app/src/main/java/com/example/android_client/activities/MainPage.java
@@ -1,12 +1,15 @@
 package com.example.android_client.activities;
 
-import android.content.res.AssetManager;
-import android.net.Uri;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.appcompat.widget.SwitchCompat;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -17,38 +20,65 @@ import com.example.android_client.entities.User;
 import com.example.android_client.entities.Video;
 import com.example.android_client.entities.VideoPreview;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-import java.util.ArrayList;
-
 import android.graphics.drawable.Drawable;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SearchView;
 import android.widget.Switch;
 import android.widget.TextView;
-
 import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.DividerItemDecoration;
 
+import java.util.ArrayList;
 
 public class MainPage extends AppCompatActivity {
 
+    boolean nightMode;
+    private SwitchCompat switchMode;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
     private RecyclerView videoList;
     private ArrayList<VideoPreview> videos;
     private TextView welcomeMessage;
     private SearchView searchInput;
-    private Switch changThemeSwitch;
     private ImageView displayImage;
     private CardView imageContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
+
+        switchMode = findViewById(R.id.darkModeSwitch);
+
+        sharedPreferences = getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMode = sharedPreferences.getBoolean("nightMode", false);
+
+        if (nightMode) {
+            switchMode.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else {
+            switchMode.setChecked(false);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+        switchMode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (switchMode.isChecked()) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode", false);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor = sharedPreferences.edit();
+                    editor.putBoolean("nightMode", true);
+                }
+                editor.apply();
+            }
+        });
+
         videoList = findViewById(R.id.recyclerView);
         videoList.setLayoutManager(new LinearLayoutManager(this));
-        changThemeSwitch = findViewById(R.id.darkModeSwitch);
         welcomeMessage = findViewById(R.id.welcomeMessage);
         displayImage = findViewById(R.id.userImage);
         imageContainer = findViewById(R.id.userImageContainer);
@@ -56,9 +86,8 @@ public class MainPage extends AppCompatActivity {
         VideoAdapter adapter = new VideoAdapter(this, videos);
         videoList.setAdapter(adapter);
         searchInput = findViewById(R.id.searchBar);
-        searchInput.setOnSearchClickListener(l -> {
-            Log.w("CLICK", "CLICK");
-        });
+
+        searchInput.setOnSearchClickListener(l -> Log.w("CLICK", "CLICK"));
         searchInput.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String s) {
@@ -79,13 +108,13 @@ public class MainPage extends AppCompatActivity {
             }
         });
 
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(videoList.getContext(),
-                DividerItemDecoration.VERTICAL);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(videoList.getContext(), DividerItemDecoration.VERTICAL);
         Drawable dividerDrawable = ContextCompat.getDrawable(this, R.drawable.divider);
         if (dividerDrawable != null) {
             dividerItemDecoration.setDrawable(dividerDrawable);
             videoList.addItemDecoration(dividerItemDecoration);
         }
+
     }
 
     @Override
