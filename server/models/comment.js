3e22fd65ede39194d5192849f25e166309cef216
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-26 13:27:27 +0300

    added schemas

---
+++ server/models/comment.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const CommentSchema = new mongoose.Schema({
+  user: { type: ObjectId, required: true },
+  video: { type: ObjectId, required: true },
+  text: { type: String, required: true },
+  date: { type: Date, default: Date.now() },
+});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+module.exports = Comment;
---
+++ server/models/video.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const VideoSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  uploader: { type: ObjectId, required: true },
+  src: { type: String, required: true },
+  likes: { type: [ObjectId], default: [] },
+  views: { type: Number, default: 0 },
+  date: { type: Date, default: Date.now() },
+  description: { type: String, default: "" },
+  tags: { type: [String], default: [] },
+  comments: { type: [ObjectId], default: [] },
+});
+
+const Video = mongoose.model("Video", VideoSchema);
+module.exports = Video;
