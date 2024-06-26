05a8630edd9372081dfcaf5901ddd70089df3c2f
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-26 13:14:10 +0300

    initializing the models

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,13 @@
 const express = require("express");
+const User = require("../models/user");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.send("users");
 });
 
+router.get("/test", async (req, res) => {
+  const all = await User.find({});
+  
+});
 module.exports = router;
