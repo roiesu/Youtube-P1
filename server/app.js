0955ee45fb6df5be6d153b893defb525214e043a
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-06-26 13:14:10 +0300

    initializing the models

--- server/app.js
+++ server/app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const router = require("./routes/router");
+const mongoose = require("mongoose");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", router);
 
-app.listen(8080);
+app.listen(8080, () => {
+  mongoose
+    .connect("mongodb+srv://orenb99:org05101971@bishbash.3blxfdz.mongodb.net/")
+    .then(() => {
+      console.log("connected");
+    })
+    .catch((err) => console.error("Database connection error:", err));
+});
