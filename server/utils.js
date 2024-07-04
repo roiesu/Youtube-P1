8680b63d76689951838371a31e80546657ce512b
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-07-05 02:18:28 +0300

    fixed the api and added videos

--- server/utils.js
+++ server/utils.js
@@ -3,21 +3,25 @@ const fs = require("fs");
 
 function write64FileWithCopies(dest, data) {
   let fileName = dest;
-  let [header, base64File] = data.split(/;base64,/);
-  let [fileType, fileSuffix] = header.replace(/data:/, "").split("/");
-  let file = fs.existsSync(`./public/${fileType}/${fileName}.${fileSuffix}`);
-  if (file) {
-    let i = 0;
-    let startName = fileName;
-    while (file) {
-      file = fs.existsSync(`./public/${fileType}/${startName} (${++i}).${fileSuffix}`);
+  try {
+    let [header, base64File] = data.split(/;base64,/);
+    let [fileType, fileSuffix] = header.replace(/data:/, "").split("/");
+    let file = fs.existsSync(`./public/${fileType}/${fileName}.${fileSuffix}`);
+    if (file) {
+      let i = 0;
+      let startName = fileName;
+      while (file) {
+        file = fs.existsSync(`./public/${fileType}/${startName} (${++i}).${fileSuffix}`);
+      }
+      fileName = `${startName} (${i})`;
     }
-    fileName = `${startName} (${i})`;
+    fs.writeFileSync(`./public/${fileType}/${fileName}.${fileSuffix}`, base64File, {
+      encoding: "base64",
+    });
+    return fileName + "." + fileSuffix;
+  } catch (err) {
+    return null;
   }
-  fs.writeFileSync(`./public/${fileType}/${fileName}.${fileSuffix}`, base64File, {
-    encoding: "base64",
-  });
-  return fileName + "." + fileSuffix;
 }
 function override64File(fileType, name, data) {
   try {
