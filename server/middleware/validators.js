669ed1b55ad65c932d7984995641692eb796005e
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-07-07 23:07:01 +0300

    fixed the validation

--- server/middleware/validators.js
+++ server/middleware/validators.js
@@ -2,7 +2,7 @@ const usernameValidator = {
   validator: (value) => {
     return value.match(/^[\w\d!@#$%^&*-_]{6,}$/);
   },
-  message: "Invalid username",
+  message: "Must be above 6 letters, only letters numbers and special symbols.",
 };
 const passwordValidator = {
   validator: (value) => {
@@ -10,12 +10,13 @@ const passwordValidator = {
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!?#$%&*+,\-./@^_{}|~])[\w\d!?#$%&*+,\-./@^{}|~]{8,}$/
     );
   },
-  message: "Invalid password",
+  message:
+    "Must be above 8 letters, must contain a capital and a small letter, a number and a special symbol.",
 };
 const nameValidator = {
   validator: (value) => {
     return value.match(/^[\w\d-_!@#$%^&*]+( [\w\d-_!@#$%^&*]+)*$/);
   },
-  message: "Invalid name",
+  message: "Contains only letters, numbers, spaces and special symbols.",
 };
 module.exports = { usernameValidator, passwordValidator, nameValidator };
