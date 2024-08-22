7b29ef684dfdff5cf450c47193f3022758b98fe2
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-08-22 17:04:46 +0300

    revert to noraml

--- android_client/app/src/main/java/com/example/android_client/datatypes/VideoWithUser.java
+++ android_client/app/src/main/java/com/example/android_client/datatypes/VideoWithUser.java
@@ -15,8 +15,8 @@ public class VideoWithUser extends Video {
     @Relation(parentColumn = "uploaderId", entityColumn = "_id", projection = {"_id", "username", "name", "image"})
     private User uploader;
 
-    public VideoWithUser(String _id, String name, String src, Integer likesCount, String thumbnail, long duration, long views, Date date, String description, ArrayList<String> tags, Integer commentsCount, User uploader) {
-        super(_id, name, null, src, likesCount, thumbnail, duration, views, date, description, tags,commentsCount);
+    public VideoWithUser(String _id, String name, String src, Integer likesNum, String thumbnail, long duration, long views, Date date, String description, ArrayList<String> tags, Integer commentsNum, User uploader) {
+        super(_id, name, null, src, likesNum, thumbnail, duration, views, date, description, tags,commentsNum);
         this.uploader = uploader;
     }
 
