77546c9333edc5bcb52aa09570b322c2a3e41c53
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-09-02 14:16:10 +0300

    error handling
    fixed some errors and crashes
    the video that's being watched would not be recommended while watching it

--- server/tcpClient.js
+++ server/tcpClient.js
@@ -1,21 +1,29 @@
 const net = require("node:net");
 const client = new net.Socket();
 
-function sendMessageToTcpServer(message) {
-    return new Promise((resolve, reject) => {
-      // Set up an event listener for TCP data
-      client.once('data', (data) => {
-        resolve(data.toString()); // Resolve with the TCP server's response
-      });
-  
-      // Handle any errors that occur
-      client.once('error', (err) => {
-        reject(err);
-      });
-  
-      // Send the message to the TCP server
-      client.write(message);
-    });
-  }
+client.on("error", (err) => {
+  console.log("couldn't connect to the tcp server:", err.message);
+});
 
-module.exports = {client,sendMessageToTcpServer};
+client.on("close", () => {
+  console.log("Disconnected from the TCP server");
+});
+
+function sendMessageToTcpServer(message) {
+  return new Promise((resolve, reject) => {
+    // Set up an event listener for TCP data
+    client.once("data", (data) => {
+      resolve(data.toString()); // Resolve with the TCP server's response
+    });
+
+    // Handle any errors that occur
+    client.once("error", (err) => {
+      reject(err);
+    });
+
+    // Send the message to the TCP server
+    client.write(message);
+  });
+}
+
+module.exports = { client, sendMessageToTcpServer };
