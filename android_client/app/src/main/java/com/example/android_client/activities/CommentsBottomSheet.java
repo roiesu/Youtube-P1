93ef70b22c7a1ec3f71be73469209e15e71dec39
orenb99 <77238204+orenb99@users.noreply.github.com>
2024-09-04 21:21:25 +0300

    open the comments in a different section

---
+++ android_client/app/src/main/java/com/example/android_client/activities/CommentsBottomSheet.java
@@ -0,0 +1,111 @@
+package com.example.android_client.activities;
+
+import static android.content.Context.INPUT_METHOD_SERVICE;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentManager;
+
+import com.example.android_client.ContextApplication;
+import com.example.android_client.DataManager;
+import com.example.android_client.R;
+import com.example.android_client.adapters.CommentAdapter;
+import com.example.android_client.view_models.CommentListViewModel;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CommentsBottomSheet extends BottomSheetDialogFragment {
+    private Context context;
+    private RecyclerView commentsRecyclerView;
+    private String videoUploader;
+    private String videoId;
+    private MutableLiveData<Integer> commentListSize;
+    private CommentListViewModel commentListViewModel;
+    private Button commentButton;
+    private TextView commentsHeader;
+    private EditText commentInput;
+    private ImageView goBack;
+
+    public CommentsBottomSheet(Context context, String videoUploader, String videoId) {
+        this.context = context;
+        this.videoUploader = videoUploader;
+        this.videoId = videoId;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.comments_list, container, false);
+        commentsRecyclerView = view.findViewById(R.id.commentsList);
+        commentButton = view.findViewById(R.id.commentButton);
+        commentsHeader = view.findViewById(R.id.commentsTitle);
+        commentInput = view.findViewById(R.id.commentInput);
+        goBack = view.findViewById(R.id.backButton);
+        goBack.setOnClickListener(l-> dismissAllowingStateLoss());
+
+        commentListSize = new MutableLiveData<>(0);
+        commentListViewModel = new CommentListViewModel();
+        CommentAdapter adapter = new CommentAdapter(context, new ArrayList<>(), this, videoUploader, commentListSize);
+        commentsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+        commentsRecyclerView.setAdapter(adapter);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(context, DividerItemDecoration.VERTICAL);
+        Drawable dividerDrawable = ContextCompat.getDrawable(context, R.drawable.divider);
+        dividerItemDecoration.setDrawable(dividerDrawable);
+        commentsRecyclerView.addItemDecoration(dividerItemDecoration);
+        commentListViewModel.getComments().observe(this, commentsList -> {
+            if (commentsList == null) {
+                return;
+            }
+            adapter.setComments(commentsList);
+            adapter.notifyDataSetChanged();
+            commentListSize.setValue(commentsList.size());
+            commentButton.setOnClickListener(l -> addComment(adapter));
+
+            commentListViewModel.getComments().setValue(null);
+        });
+        commentListSize.observe(this, count -> {
+            commentsHeader.setText(count + " Comments");
+        });
+        commentListViewModel.getCommentsByVideo(videoId);
+
+        return view;
+    }
+
+    private void addComment(CommentAdapter adapter) {
+        if (DataManager.getCurrentUsername() == null) {
+            ContextApplication.showToast("Can't comment without login");
+            return;
+        }
+        adapter.addComment(commentInput, videoId, commentsRecyclerView);
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(commentInput.getWindowToken(), 0);
+    }
+
+
+    // Method to show the BottomSheetDialogFragment
+    public void show(FragmentManager fragmentManager) {
+        this.show(fragmentManager, "CommentsBottomSheet");
+    }
+}
