df7bb416efe0c56053e6949512ecae6a34cc85a3
roiesu <roiesu3@gmail.com>
2025-02-11 16:26:00 +0200

    fixes

--- server/app.js
+++ server/app.js
@@ -12,11 +12,11 @@ const path = require("path");
 app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "500mb" }));
-if(!fs.existsSync("./public/image")){
+if(!fs.existsSync("./public/video")){
   fs.mkdirSync("./public/video",{recursive:true});
 }
 if(!fs.existsSync("./public/image")){
-  fs.mkdirSync("./public/video",{recursive:true});
+  fs.mkdirSync("./public/image",{recursive:true});
 }
 app.use("/media", express.static("./public"));
 app.use("/api", router);
